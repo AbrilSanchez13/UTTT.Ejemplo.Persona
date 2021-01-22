4c8039571d3de27df76c61acf96f2badeaf01393
abrilSanchez <abril.meb@gmail.com>
2021-01-21 21:46:21 -0600

    Crud

---
+++ UTTT.Ejemplo.Persona.Control/Ctrl/Conexion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UTTT.Ejemplo.Persona.Control.Ctrl
+{
+    public class Conexion
+    {
+        #region Variables
+     
+        #endregion
+
+        #region Constructores
+        public Conexion()
+        {
+        }       
+        #endregion
+
+
+        public SqlConnection sqlConnection()
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection("Data Source=sql5052.site4now.net;Initial Catalog=DB_A6E0A7_AbrilSanchez13;Persist Security Info=True;User ID=DB_A6E0A7_AbrilSanchez13_admin;Password=1kerbadillO");
+                return conexion;
+            }
+            catch (Exception _e)
+            { 
+            
+            }
+            return null;
+        }
+    }
+}
